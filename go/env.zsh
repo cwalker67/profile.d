5b7ebd09820870f8a456fad30f93e092f2c1e382
Charles Walker <charlie@walkersworld.org>
2016-12-19 13:01:49 -0500

    add go to environment

---
+++ go/env.zsh
@@ -0,0 +1,4 @@
+GOPATH=$HOME/workspaces/go
+PATH=$PATH:/usr/local/opt/go/libexec/bin
+
+export GOPATH PATH
--- setEnv.zsh
+++ setEnv.zsh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 SCRIPTDIR=${0:a:h}
-for dir in 'development' 'docker' 'java' 'zsh' 'homebrew'
+for dir in 'development' 'docker' 'java' 'zsh' 'homebrew' 'go'
 do
     for zshFile in `find $SCRIPTDIR/$dir -iname "*.zsh"`
     do
